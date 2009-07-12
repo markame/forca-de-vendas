749881cfd34681f431fd4e3ada8f4720549d527a
decofreitas <decofreitas@fd9b6f72-68f5-11de-9b35-9190732fe600>
2009-07-12 15:39:39 +0000

    Importada a aplicação cliente do componente Cliente

---
+++ ClienteComponente/src/java/clientecomponente/EJBUtil.java
@@ -0,0 +1,50 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientecomponente;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+//import com.sun.enterprise.naming.SerialInitContextFactory;
+
+/**
+ *
+ * @author Henrique
+ */
+public class EJBUtil {
+
+    private static Properties getProperties() {
+        //Propriedades do initial context.  
+        Properties props = new Properties();
+        props.setProperty("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+        //props.setProperty("java.naming.provider.url", "ecompjrserver.uefs.br:3306");
+        props.setProperty("java.naming.provider.url", "localhost:3700");  
+        props.setProperty("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+
+        props.setProperty("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+
+        return props;
+    }
+
+    public static Object getFacade(String facadeClass) {
+        Object facade = null;
+
+        try {
+            InitialContext ctx = new InitialContext(getProperties());
+            //InitialContext ctx = new InitialContext();
+
+            //Quando vc faz o lookup vc passa o caminho completo da interface local ou remote.
+            facade = ctx.lookup(facadeClass);
+
+        } catch (NamingException ex) {
+            Logger.getLogger("ComponenteCliente").log(Level.SEVERE, "Conexão EJB falhou: " + facadeClass + " \n" +
+                    ex.toString());
+        }
+
+        return facade;
+    }
+}
