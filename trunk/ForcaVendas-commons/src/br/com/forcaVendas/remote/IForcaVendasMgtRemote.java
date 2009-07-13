8189043f6068cfc1244f3a069f059187a9bc4c51
decofreitas <decofreitas@fd9b6f72-68f5-11de-9b35-9190732fe600>
2009-07-13 03:20:53 +0000

    Atualizar o Cliente e criado o componente maior que interliga tudo...

---
+++ trunk/ForcaVendas-commons/src/br/com/forcaVendas/remote/IForcaVendasMgtRemote.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.forcaVendas.remote;
+
+import br.com.forcaVendas.dto.ClienteDTO;
+import br.com.forcaVendas.dto.EmpresaDTO;
+import br.com.forcaVendas.dto.Fornecedor;
+import br.com.forcaVendas.dto.ItemDTO;
+import br.com.forcaVendas.dto.PedidoDTO;
+import br.com.forcaVendas.dto.PedidoItemDTO;
+import br.com.forcaVendas.dto.Solicitacao;
+import br.com.forcaVendas.dto.VendedorDTO;
+import br.com.forcaVendas.empresa.remote.EmpresaException;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author André
+ */
+@Remote
+public interface IForcaVendasMgtRemote {
+
+    //IAtualizarCliente
+    public boolean createCliente(String nome, String endereco, String cpf, String telefone);
+
+    public ClienteDTO getCliente(String cpf);
+
+    public List<ClienteDTO> getClientes();
+
+    public boolean deleteCliente(String cpf);
+
+    public boolean updateCliente(ClienteDTO cliente);
+
+    //os metodos de Fatura
+
+    //IAtualizarVendedor
+    public VendedorDTO createVendedor(String nome, String endereco, String telefone, long cpf, float salario) throws EmpresaException;
+
+    public VendedorDTO getVendedor(long codigo) throws EmpresaException;
+
+    public List<VendedorDTO> getVendedores() throws EmpresaException;
+
+    public boolean deleteVendedor(long codigo) throws EmpresaException;
+
+    public boolean updateVendedor(VendedorDTO vendedor) throws EmpresaException;
+
+    //IAtualizarItem
+    public ItemDTO createItem(String nome, float preco) throws EmpresaException;
+
+    public ItemDTO getItem(long codigo) throws EmpresaException;
+
+    public List<ItemDTO> getItens() throws EmpresaException;
+
+    public boolean updateItem(ItemDTO itemDTO) throws EmpresaException;
+
+    public boolean deleteItem(long codigo) throws EmpresaException;
+
+    //ISolicitarItem
+    public Solicitacao solicitarItem(List<Integer> itens, EmpresaDTO empresa, Fornecedor fornecedor);
+
+    //public List<ItemDTO> getItens() throws EmpresaException;
+
+    public Fornecedor getFornecedor(Integer id);
+
+    public EmpresaDTO getEmpresa() throws EmpresaException;
+
+    //IFazerPedido
+    //public ClienteDTO getCliente(String cpf);
+
+    //public List<ItemDTO> getItens() throws EmpresaException;
+
+    //public ItemDTO getItem(long codigo) throws EmpresaException; - seria o metodo addItem()
+
+    //public VendedorDTO getVendedor(long codigo) throws EmpresaException;
+
+    //orderItem();
+
+    //Fazer pedido não foi definido em nenhuma interface
+    public PedidoDTO fazerPedido(long cliente, VendedorDTO vendedor, List<PedidoItemDTO> itens) throws EmpresaException;
+}
---
+++ trunk/ForcaVendasEJB/src/java/br/com/forcaVendas/ForcaVendasMgr.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.forcaVendas;
+
+import br.com.forcaVendas.cliente.remote.IClienteMgtRemote;
+import br.com.forcaVendas.dto.ClienteDTO;
+import br.com.forcaVendas.dto.EmpresaDTO;
+import br.com.forcaVendas.dto.Fornecedor;
+import br.com.forcaVendas.dto.ItemDTO;
+import br.com.forcaVendas.dto.PedidoDTO;
+import br.com.forcaVendas.dto.PedidoItemDTO;
+import br.com.forcaVendas.dto.Solicitacao;
+import br.com.forcaVendas.dto.VendedorDTO;
+import br.com.forcaVendas.empresa.remote.EmpresaException;
+import br.com.forcaVendas.empresa.remote.IEmpresaMgtRemote;
+import br.com.forcaVendas.fornecedor.remote.IFornecedorMgt;
+import br.com.forcaVendas.remote.IForcaVendasMgtRemote;
+import java.util.List;
+import java.util.Vector;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author André
+ */
+@Stateless
+public class ForcaVendasMgr implements IForcaVendasMgtRemote{
+
+    @EJB
+    public IClienteMgtRemote clienteMgtRemote = null;
+
+    @EJB
+    public IEmpresaMgtRemote empresaMgtRemote = null;
+
+    @EJB
+    public IFornecedorMgt fornecedorMgtRemote = null;
+
+    //IAtualizarCliente
+    public boolean createCliente(String nome, String endereco, String cpf, String telefone) {
+        return clienteMgtRemote.criarCliente(nome, endereco, cpf, telefone);
+    }
+
+    public ClienteDTO getCliente(String cpf) {
+        return clienteMgtRemote.buscarCliente(cpf);
+    }
+
+    public List<ClienteDTO> getClientes() {
+        return clienteMgtRemote.getClientes();
+    }
+
+    public boolean deleteCliente(String cpf) {
+        return clienteMgtRemote.deletarCliente(cpf);
+    }
+
+    public boolean updateCliente(ClienteDTO cliente) {
+        return clienteMgtRemote.editarCliente(cliente);
+    }
+    //Fim de IAtualizarCliente
+
+    //IAtualizarVendedor
+    public VendedorDTO createVendedor(String nome, String endereco, String telefone, long cpf, float salario) throws EmpresaException {
+        return empresaMgtRemote.createVendedor(nome, endereco, telefone, cpf, salario);
+    }
+
+    public VendedorDTO getVendedor(long codigo) throws EmpresaException {
+        return empresaMgtRemote.getVendedor(codigo);
+    }
+
+    public List<VendedorDTO> getVendedores() throws EmpresaException {
+        return empresaMgtRemote.getVendedores();
+    }
+
+    public boolean deleteVendedor(long codigo) throws EmpresaException {
+        return empresaMgtRemote.deleteVendedor(codigo);
+    }
+
+    public boolean updateVendedor(VendedorDTO vendedor) throws EmpresaException {
+        return empresaMgtRemote.updateVendedor(vendedor);
+    }
+    //Fim de IAtualizarVendedor
+
+    //IAtualizarItem
+    public ItemDTO createItem(String nome, float preco) throws EmpresaException {
+        return empresaMgtRemote.createItem(nome, preco);
+    }
+
+    public ItemDTO getItem(long codigo) throws EmpresaException {
+        return empresaMgtRemote.getItem(codigo);
+    }
+
+    public List<ItemDTO> getItens() throws EmpresaException {
+        return empresaMgtRemote.getItens();
+    }
+
+    public boolean updateItem(ItemDTO itemDTO) throws EmpresaException {
+        return empresaMgtRemote.updateItem(itemDTO);
+    }
+
+    public boolean deleteItem(long codigo) throws EmpresaException {
+        return empresaMgtRemote.deleteItem(codigo);
+    }
+    //Fim de IAtualizarItem
+
+    //ISolicitarItem
+    public Solicitacao solicitarItem(List<Integer> itens, EmpresaDTO empresa, Fornecedor fornecedor) {
+        return fornecedorMgtRemote.solicitarItem(itens, empresa, fornecedor);
+    }
+
+    public Fornecedor getFornecedor(Integer id) {
+        return fornecedorMgtRemote.getFornecedor(id);
+    }
+
+    public EmpresaDTO getEmpresa() throws EmpresaException {
+        return empresaMgtRemote.getEmpresa();
+    }
+    //Fim de ISolicitarItem
+
+    //Fazer pedido não foi definido em nenhuma interface
+    public PedidoDTO fazerPedido(long cliente, VendedorDTO vendedor, List<PedidoItemDTO> itens) throws EmpresaException {
+        Vector<Integer> itensFaltaEstoque = new Vector<Integer>();
+
+        for(PedidoItemDTO item : itens){
+            ItemDTO itemEstoque = empresaMgtRemote.getItem(item.getCodigo());
+            if(itemEstoque == null){
+                itensFaltaEstoque.add(new Integer(itemEstoque.getCodigo().toString()));
+            }
+        }
+        if(!itensFaltaEstoque.isEmpty()){
+            fornecedorMgtRemote.solicitarItem(itensFaltaEstoque, empresaMgtRemote.getEmpresa(), fornecedorMgtRemote.getFornecedor(Integer.SIZE));
+        }
+
+        return empresaMgtRemote.fazerPedido(cliente, vendedor, itens);
+    }
+    
+}
