acfed9f22aa2ddc213c65118e6d00f27576828dc
nelsonafn <nelsonafn@fd9b6f72-68f5-11de-9b35-9190732fe600>
2009-07-15 13:17:43 +0000

    Teste da interface do módulo fornecedor(incompleto)

--- FornecedorComponente/test/fornecedor/IFornecedorMgtTest.java
+++ FornecedorComponente/test/fornecedor/IFornecedorMgtTest.java
@@ -10,6 +10,7 @@ import br.com.forcaVendas.dto.EmpresaDTO;
 import br.com.forcaVendas.dto.Fornecedor;
 import br.com.forcaVendas.dto.Solicitacao;
 import fornecedor.interfaces.remote.IFornecedorMgt;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,6 +30,11 @@ import static org.junit.Assert.*;
  */
 public class IFornecedorMgtTest {
 
+    private Fornecedor fornecedor1;
+    private Fornecedor fornecedor2;
+    private EmpresaDTO empresa;
+    private Solicitacao solicitacao;
+
     public IFornecedorMgtTest() {
     }
 
@@ -40,83 +46,116 @@ public class IFornecedorMgtTest {
     public static void tearDownClass() throws Exception {
     }
 
-    @Before
+        @Before
     public void setUp() {
+        //cria fornecedor 1
+        this.fornecedor1 = new Fornecedor();
+        fornecedor1.setCnpj(123456789);
+        fornecedor1.setEndereco("Rua A");
+        fornecedor1.setId(1);
+        fornecedor1.setNome("UEFS");
+        fornecedor1.setTelefone("91181703");
+
+        //cria fornecedor 2
+        this.fornecedor2 = new Fornecedor();
+        fornecedor2.setCnpj(987654321);
+        fornecedor2.setEndereco("Rua B");
+        fornecedor2.setId(2);
+        fornecedor2.setNome("SBC");
+        fornecedor2.setTelefone("32352103");
+
+        //cria uma empresa
+        this.empresa = new EmpresaDTO();
+        empresa.setCnpj(123098456);
+        empresa.setEndereco("Rua abc");
+        empresa.setId(1);
+        empresa.setNome("blabla");
+        empresa.setTelefone("1234566");
+
+        //cria solicitação
+        this.solicitacao = new Solicitacao();
+        solicitacao.setId(1);
+        solicitacao.setIdEmpresa(this.empresa.getId());
+        solicitacao.setIdFornecedor(this.fornecedor1.getId());
+
     }
 
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of getFornecedor method, of class IFornecedorMgt.
-     */
-    @Test
-    public void testGetFornecedor() {
-        System.out.println("getFornecedor");
-        Integer id = null;
-        IFornecedorMgt instance = lookupFornecedorMgt();
-        Fornecedor expResult = null;
-        Fornecedor result = instance.getFornecedor(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
-     * Test of solicitarItem method, of class IFornecedorMgt.
-     */
-    @Test
-    public void testSolicitarItem() {
-        System.out.println("solicitarItem");
-        List<Integer> itens = null;
-        EmpresaDTO empresa = null;
-        Fornecedor fornecedor = null;
-        IFornecedorMgt instance = lookupFornecedorMgt();
-        Solicitacao expResult = null;
-        Solicitacao result = instance.solicitarItem(itens, empresa, fornecedor);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of createFornecedor method, of class IFornecedorMgt.
+     * Test of createFornecedor method, of class FornecedorMgt.
      */
     @Test
     public void testCreateFornecedor() {
         System.out.println("createFornecedor");
-        Fornecedor fornecedor = null;
         IFornecedorMgt instance = lookupFornecedorMgt();
-        instance.createFornecedor(fornecedor);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.createFornecedor(this.fornecedor1);
+        instance.createFornecedor(this.fornecedor2);
+
+        int id = 1;
+        Fornecedor result = instance.getFornecedor(id);
+        assertEquals(this.fornecedor1, result);
     }
 
-    /**
-     * Test of deleteFornecedor method, of class IFornecedorMgt.
-     */
-    @Test
-    public void testDeleteFornecedor() {
-        System.out.println("deleteFornecedor");
-        Integer id = null;
-        IFornecedorMgt instance = lookupFornecedorMgt();
-        instance.deleteFornecedor(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of updateFornecedor method, of class IFornecedorMgt.
+     /**
+     * Test of updateFornecedor method, of class FornecedorMgt.
      */
     @Test
     public void testUpdateFornecedor() {
         System.out.println("updateFornecedor");
-        Fornecedor fornecedor = null;
+        fornecedor1.setEndereco("Rua AA BB DD");
         IFornecedorMgt instance = lookupFornecedorMgt();
-        instance.updateFornecedor(fornecedor);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.updateFornecedor(fornecedor1);
+
+        int id = 1;
+        Fornecedor result = instance.getFornecedor(id);
+        assertEquals(this.fornecedor1, result);
+    }
+
+
+    /**
+     * Test of getFornecedor method, of class FornecedorMgt.
+     */
+    @Test
+    public void testGetFornecedor() {
+        System.out.println("getFornecedor");
+        Integer id = 1;
+        IFornecedorMgt instance = lookupFornecedorMgt();
+        Fornecedor result = instance.getFornecedor(id);
+        assertEquals(this.fornecedor1, result);
+        id = 2;
+        result = instance.getFornecedor(id);
+        assertEquals(this.fornecedor2, result);
+    }
+
+    /**
+     * Test of solicitarItem method, of class FornecedorMgt.
+     */
+    @Test
+    public void testSolicitarItem() {
+        System.out.println("solicitarItem");
+        List<Integer> itens = new ArrayList<Integer>();
+        itens.add(12); itens.add(32);
+        IFornecedorMgt instance = lookupFornecedorMgt();
+        Solicitacao result = instance.solicitarItem(itens, empresa, fornecedor1);
+        assertEquals(solicitacao, result);
+
+    }
+
+    /**
+     * Test of deleteFornecedor method, of class FornecedorMgt.
+     */
+    @Test
+    public void testDeleteFornecedor() {
+        System.out.println("deleteFornecedor");
+        Integer id = 1;
+        IFornecedorMgt instance = lookupFornecedorMgt();
+        instance.deleteFornecedor(id);
+
     }
 
     private IFornecedorMgt lookupFornecedorMgt() {
@@ -128,6 +167,8 @@ public class IFornecedorMgtTest {
             throw new RuntimeException(ne);
         }
     }
+
+    
 
 
 
