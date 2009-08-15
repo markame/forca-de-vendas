7699e9ad58ee4eff48eb2fe83bf2840b9c84d060
henriquebrandao.fs <henriquebrandao.fs@fd9b6f72-68f5-11de-9b35-9190732fe600>
2009-08-15 15:37:15 +0000

    Criação de métodos síncronos em EmpresaMgtRemote para atualizar a quantidade em estoque.

--- trunk/ForcaVendas-commons/src/br/com/forcaVendas/empresa/remote/IEmpresaMgtRemote.java
+++ trunk/ForcaVendas-commons/src/br/com/forcaVendas/empresa/remote/IEmpresaMgtRemote.java
@@ -16,9 +16,21 @@ import javax.ejb.Remote;
 @Remote
 public interface IEmpresaMgtRemote {
 
-
+    /**
+     * Retorna os dados da empresa que utiliza o sistema
+     *
+     * @return
+     * @throws EmpresaException
+     */
     public EmpresaDTO getEmpresa() throws EmpresaException;
 
+    /**
+     * Atualiza os dados da empresa que utiliza o sistema
+     * 
+     * @param empresa
+     * @return
+     * @throws EmpresaException
+     */
     public boolean setEmpresa(EmpresaDTO empresa) throws EmpresaException;
 
     public VendedorDTO createVendedor(String nome, String endereco, String telefone, long cpf, float salario) throws EmpresaException;
@@ -31,6 +43,16 @@ public interface IEmpresaMgtRemote {
 
     public List<VendedorDTO> getVendedores() throws EmpresaException;
 
+    /**
+     * Efetua o pedido de um cliente e atualiza as quantidades em estoque.
+     *
+     *
+     * @param cliente
+     * @param vendedor
+     * @param itens
+     * @return
+     * @throws EmpresaException
+     */
     public PedidoDTO fazerPedido(int cliente, VendedorDTO vendedor, List<PedidoItemDTO> itens) throws EmpresaException;
 
     public PedidoDTO getPedido(int codigo) throws EmpresaException;
@@ -48,14 +70,75 @@ public interface IEmpresaMgtRemote {
                         Double valorImpostos,
                         String dadosAdicionais) throws EmpresaException;
 
+    /**
+     * cria um novo Item no banco
+     *
+     * @param nome
+     * @param preco
+     * @return
+     * @throws EmpresaException
+     */
     public ItemDTO createItem(String nome, float preco) throws EmpresaException;
 
+    /**
+     * Seleciona um item pelo seu código
+     *
+     * @param codigo
+     * @return
+     * @throws EmpresaException
+     */
     public ItemDTO getItem(int codigo) throws EmpresaException;
 
+    /**
+     * Lista todos os itens
+     *
+     * @return
+     * @throws EmpresaException
+     */
     public List<ItemDTO> getItens() throws EmpresaException;
 
+    /**
+     * Atualiza o item na tabela.
+     * 
+     * OBS: não é recomendável alterar o estoque através deste método.
+     *
+     * @param itemDTO
+     * @return
+     * @throws EmpresaException
+     */
     public boolean updateItem(ItemDTO itemDTO) throws EmpresaException;
 
+    /**
+     * Remove itens do banco
+     *
+     * @param codigo
+     * @return
+     * @throws EmpresaException
+     */
     public boolean deleteItem(int codigo) throws EmpresaException;
+
+    
+    /**
+     * Adiciona itens ao estoque
+     * criada para manter em um lugar só a alteração do estoque
+     * 
+     * @param codigo
+     * @param quantidade
+     * @return
+     * @throws EmpresaException
+     */
+    public ItemDTO incrementarEstoqueItem(int codigo, float quantidade) throws EmpresaException;
+
+    
+    /**
+     * Remove itens no estoque
+     * criada para manter em um lugar só a alteração do estoque
+     *  
+     * @param codigo
+     * @param quantidade
+     * @return
+     * @throws EmpresaException
+     */
+    public ItemDTO decrementarEstoqueItem(int codigo, float quantidade) throws EmpresaException;
 
 }
